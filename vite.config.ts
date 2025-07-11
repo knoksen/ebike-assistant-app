08631fd73c7dfc3cf12d3bc7c060fa8fcb427d6e
Jarle Taksdal <31391420+knoksen@users.noreply.github.com>
2025-07-11 23:24:13 +0200

    Add PWA manifest configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,20 @@
 import { defineConfig, type PluginOption } from 'vite'
 import react from '@vitejs/plugin-react'
 import { visualizer } from 'rollup-plugin-visualizer'
+import { VitePWA } from 'vite-plugin-pwa'
+import { createRequire } from 'module'
 
 // https://vite.dev/config/
 export default defineConfig(() => {
-  const plugins: PluginOption[] = [react()]
+  const require = createRequire(import.meta.url)
+  const plugins: PluginOption[] = [
+    react(),
+    VitePWA({
+      srcDir: 'public',
+      filename: 'manifest.webmanifest',
+      manifest: require('./public/manifest.json'),
+    }),
+  ]
   if (process.env.ANALYZE) {
     plugins.push(
       visualizer({
