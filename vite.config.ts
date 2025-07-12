39dda22ac0c20b5eedd8c47c54f93df9b186f725
Jarle Taksdal <31391420+knoksen@users.noreply.github.com>
2025-07-12 09:38:08 +0200

    parse manifest manually

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,18 @@
 import { defineConfig, type PluginOption } from 'vite'
 import react from '@vitejs/plugin-react'
-import { createRequire } from 'node:module'
+import { readFileSync } from 'node:fs'
 import { visualizer } from 'rollup-plugin-visualizer'
 import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig(() => {
-  const require = createRequire(import.meta.url)
+  const manifest = JSON.parse(
+    readFileSync(new URL('./public/manifest.webmanifest', import.meta.url), 'utf-8'),
+  )
   const plugins: PluginOption[] = [
     react(),
     VitePWA({
       srcDir: 'public',
-      manifest: require('./public/manifest.webmanifest'),
+      manifest,
       manifestFilename: 'manifest.webmanifest',
       workbox: {
         globPatterns: ['**/*.{js,css,html,ico,png,svg,webmanifest}'],
