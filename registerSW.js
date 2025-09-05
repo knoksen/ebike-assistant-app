e47df94d379bf8fec568ee287f2b0c43c9fbfa36
Jarle Taksdal <31391420+knoksen@users.noreply.github.com>
2025-09-05 19:13:03 +0200

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/ebike-assistant-app/sw.js', { scope: '/ebike-assistant-app/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('./sw.js', { scope: './' })})}
