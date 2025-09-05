334f07314b8de34bdc38d8276680fe057726089c
Jarle Taksdal <31391420+knoksen@users.noreply.github.com>
2025-09-06 01:50:17 +0200

    Update BoostSettings.tsx

---
+++ src/components/BoostSettings.tsx
@@ -0,0 +1,5 @@
+// Deprecated: This file now simply re-exports the new BoostSettings2 component.
+// Kept to avoid breaking existing imports prior to refactor.
+export { BoostSettings2 as BoostSettings } from './BoostSettings2'
+export { BoostSettings2 as BoostSettingsComponent } from './BoostSettings2'
+export { default } from './BoostSettings2'
